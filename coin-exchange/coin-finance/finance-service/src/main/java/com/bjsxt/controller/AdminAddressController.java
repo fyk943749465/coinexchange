e79a3e0d9899124913311f7c1f93819c1aee3624
fengyunkai <fyk943749465@gmail.com>
2022-02-28 19:17:29 -0800

    钱包归集地址的新增和修改操作

--- coin-exchange/coin-finance/finance-service/src/main/java/com/bjsxt/controller/AdminAddressController.java
+++ coin-exchange/coin-finance/finance-service/src/main/java/com/bjsxt/controller/AdminAddressController.java
@@ -8,9 +8,8 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
@@ -30,5 +29,34 @@ public class AdminAddressController {
     public R<Page<AdminAddress>> findByPage(@ApiIgnore Page<AdminAddress> page, Long coinId) {
         Page<AdminAddress> adminAddressPage =  adminAddressService.findByPage(page, coinId);
         return R.ok(adminAddressPage);
+    }
+
+    @PostMapping
+    @ApiOperation(value = "钱包归集地址的新增操作")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "adminAddress", value = "adminAddress 的json数据")
+    })
+    public R add(@RequestBody @Validated AdminAddress adminAddress) {
+
+        boolean isOk = adminAddressService.save(adminAddress);
+        if (isOk) {
+            return R.ok();
+        }
+        return R.fail("新增失败");
+    }
+
+
+    @PatchMapping
+    @ApiOperation(value = "钱包归集地址的新增操作")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "adminAddress", value = "adminAddress 的json数据")
+    })
+    public R update(@RequestBody @Validated AdminAddress adminAddress) {
+
+        boolean isOk = adminAddressService.updateById(adminAddress);
+        if (isOk) {
+            return R.ok();
+        }
+        return R.fail("修改失败");
     }
 }
