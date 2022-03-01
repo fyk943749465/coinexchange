6f3d2db57a6a1682b9047b6d4477750b58322977
fengyunkai <fyk943749465@gmail.com>
2022-02-28 16:47:59 -0800

    查询币种的配置信息

--- coin-exchange/coin-finance/finance-service/src/main/java/com/bjsxt/service/CoinConfigService.java
+++ coin-exchange/coin-finance/finance-service/src/main/java/com/bjsxt/service/CoinConfigService.java
@@ -5,4 +5,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface CoinConfigService extends IService<CoinConfig>{
 
 
+    /**
+     * 通过币种的id查询币种的配置信息
+     * @param coinId
+     * @return
+     */
+    CoinConfig findByCoinId(Long coinId);
 }
--- coin-exchange/coin-finance/finance-service/src/main/java/com/bjsxt/service/impl/CoinConfigServiceImpl.java
+++ coin-exchange/coin-finance/finance-service/src/main/java/com/bjsxt/service/impl/CoinConfigServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bjsxt.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
@@ -10,4 +11,9 @@ import com.bjsxt.service.CoinConfigService;
 @Service
 public class CoinConfigServiceImpl extends ServiceImpl<CoinConfigMapper, CoinConfig> implements CoinConfigService{
 
+    @Override
+    public CoinConfig findByCoinId(Long coinId) {
+        // coinConfig的id和Coin中的id，是同一个id
+        return getOne(new LambdaQueryWrapper<CoinConfig>().eq(CoinConfig::getId, coinId));
+    }
 }
